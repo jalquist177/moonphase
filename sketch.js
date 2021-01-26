8490a2abe94faa657e4b6838b3e3d48e65082a08
Justin Alquist <blossom@Blossom.local>
2021-01-26 11:09:20 -0800

    updated code

--- sketch.js
+++ sketch.js
@@ -17,26 +17,7 @@ function draw() {
 background(0);
 let phase=map(mouseX, 0, width, 1, 30);
 drawMoon(phase);
-//print(mouseX+'\n');
 
-//  image(img, 550, 100, 250, 250);
-
-// {
-// //translate(width/2, height/2, 200);
-// //sphere(40);
-// //fill(255, 255, 255);
-// }
-
-// {
-// //ellipse(650, 200, 200, 200);
-// fill(0);
-// }
-
-// {
-// //ellipse(mouseX, 200, 250, 400);
-// ellipse(mouseX, 200, 250, 280);
-// fill(255, 255, 255);
-// }
 
 
 for (var i = 0; i <= numstar; i++) {
@@ -46,141 +27,6 @@ for (var i = 0; i <= numstar; i++) {
   pop();
 }
 
-// push();
-// translate(100, 100);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(300, 100);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(700, 400);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(625, 550);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(100, 600);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(200, 650);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(100, 400);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(150, 250);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(300, 500);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(250, 300);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(555, 666);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(400, 666);
-// drawStar(10);
-// pop();
-
-
-// push();
-// translate(500, 600);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(444, 333);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(222, 333);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(444, 113);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(350, 300);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(700, 700);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(666, 666);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(500, 600);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(200, 300);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(440, 444);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(440, 200);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(200, 444);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(200, 200);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(300, 444);
-// drawStar(10);
-// pop();
-
-// push();
-// translate(500, 450);
-// drawStar(10);
-// pop();
 
 
 }	
@@ -211,27 +57,17 @@ function drawMoon(phase){
   phase=map(phase, 1, 30, 450, 910);
   image(img, 550, 100, 250, 250);
 
-//{
-//translate(width/2, height/2, 200);
-//sphere(40);
-//fill(255, 255, 255);
-//}
+
 
 {
-//ellipse(650, 200, 200, 200);
+
 fill(0);
 }
 
 {
-//ellipse(mouseX, 200, 250, 400);
 ellipse(phase, 200, 250, 280);
 fill(255, 255, 255);
 }
 
 }
 
-//function drawMoon(Let moonphase){
-
-	
-
-//}
